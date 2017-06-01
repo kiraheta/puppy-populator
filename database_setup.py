65b73600874c5437a2505c8fb1da4df8902cce0c
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-05-31 22:00:00 -0700

    Add Shelter and Puppy classes

--- database_setup.py
+++ database_setup.py
@@ -1,13 +1,37 @@
-import sys
-from sqlalchemy import
-Column, ForeignKey, Integer, SUBSTRING
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
+
 Base = declarative_base()
 
+
+class Shelter(Base):
+    _tablename_ = 'shelter'
+    name = Column(String(80), nullable = False)
+    address = Column(String(250))
+    city = Column(String(80))
+    state = Column(String(20))
+    zipcode = Column(String(10))
+    website = Column(String)
+    id = Column(Integer, primary_key = True)
+
+
+class Puppy(Base):
+    _tablename_ = 'puppy'
+    name = Column(String(250), nullable=False)
+    gender = Column(String(6), nullable = False)
+    dateOfBirth = Column(Date)
+    picture = Column(String)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+    shelter = relationship(Shelter)
+    weight = Column(Numeric(10))
+    id = Column(Integer, primary_key=True)
+
+
 engine = create_engine(
-'sqlite:///puppies.db')
+'sqlite:///puppyshelter.db')
+
 
 Base.metadata.create_all(engine)
