5ad5f686924a7c39fc387f28a311860357b3e7ef
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-01 17:51:51 -0700

    Update project

--- puppies.py
+++ puppies.py
@@ -2,24 +2,22 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-
-
+ 
 Base = declarative_base()
 
-
 class Shelter(Base):
-    _tablename_ = 'shelter'
-    name = Column(String(80), nullable = False)
+    __tablename__ = 'shelter'
+    id = Column(Integer, primary_key = True)
+    name =Column(String(80), nullable = False)
     address = Column(String(250))
     city = Column(String(80))
     state = Column(String(20))
-    zipcode = Column(String(10))
+    zipCode = Column(String(10))
     website = Column(String)
-    id = Column(Integer, primary_key = True)
-
-
+    
 class Puppy(Base):
-    _tablename_ = 'puppy'
+    __tablename__ = 'puppy'
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     gender = Column(String(6), nullable = False)
     dateOfBirth = Column(Date)
@@ -27,11 +25,9 @@ class Puppy(Base):
     shelter_id = Column(Integer, ForeignKey('shelter.id'))
     shelter = relationship(Shelter)
     weight = Column(Numeric(10))
-    id = Column(Integer, primary_key=True)
 
 
-engine = create_engine(
-'sqlite:///puppyshelter.db')
-
+engine = create_engine('sqlite:///puppyshelter.db')
+ 
 
 Base.metadata.create_all(engine)
